ae2f20d31ba9751e85868a16b2531d243bb9bafd
Bijay B.K <bijaybk307@gmail.com>
2024-05-16 17:22:04 +0545

    Initial commit

--- script.js
+++ script.js
@@ -43,17 +43,71 @@
 
 //ExpressJs started
 
+// const express = require('express')
+// const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.get('/profile', function (req, res){
+//     res.send('I am Bijay don bro!')
+// })
+
+// app.listen(3000)
+
+
+//Middleware 
+
 const express = require('express')
 const app = express()
+
+app.use(function(req, res, next){
+  console.log('Middleware Chalyo');
+  next();
+})
+
+app.use(function(req, res, next){
+  console.log('Middleware feri Chalyo');
+  next();
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/profile', function (req, res){
-    res.send('I am Bijay don bro!')
+app.get('/profile', function (req, res, next){
+    return next(new Error("Something went wrong"))
+})
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
 app.listen(3000)
 
 
+
+// //json() and url encoded
+
+// const express = require('express')
+// const app = express()
+
+// app.use(express.json());
+// app.use(express.urlencoded({etended: true}));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.get('/profile', function (req, res, next){
+//     return next(new Error("Something went wrong"))
+// })
+
+// app.get("/about", function(req,res, next){
+//   return next(new Error("Not Implemented"));
+// })
+
+// app.listen(3000)
